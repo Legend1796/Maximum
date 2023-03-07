f7af6a477fe27937771211819b860d7bc02e9240
Igor Manukhov <m_igor97@mail.ru>
2023-03-08 01:06:46 +0300

    placeholder

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,17 +1,12 @@
+import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import "react-dadata/dist/react-dadata.css";
 import checkbox_active from "../../images/checkbox_active.svg";
-// import { useFormAndValidation } from "../../hooks/useFormAndValidation";
 import { useEffect, useState } from "react";
-import PhoneInput from "react-phone-input-2";
 import { FioSuggestions, EmailSuggestions } from "react-dadata";
 import "./form.css";
 
 function Form() {
-  //   const { values, handleChange, errors, resetErrors, isValidName, isValidPhone, isValidEmail } = useFormAndValidation(
-  //     {}
-  //   );
-
   const [isValid, setIsValid] = useState(false);
   const [email, setEmail] = useState();
   const [name, setName] = useState();
@@ -21,8 +16,8 @@ function Form() {
     e.preventDefault();
     console.log("submit");
   }
-  function handleChange() {
-    console.log("change");
+  function handleChange(changed) {
+    console.log(changed);
   }
 
   useEffect(() => {
@@ -31,14 +26,6 @@ function Form() {
     }
   }, [name, email, phone]);
 
-  //   useEffect(() => {
-  //     setIsValid(email && isValidPhone && name);
-  //     console.log(isValid);
-  //   }, [isValidName, isValidPhone, isValidEmail]);
-
-  //   useEffect(() => {
-  //     resetErrors({ name: "", phone: "", email: "" });
-  //   }, []);
   return (
     <section className="form">
       <div className="form__container">
@@ -47,39 +34,28 @@ function Form() {
         </div>
         <form onSubmit={handleSubmit} name="send" className="form__body">
           <div className="input__container">
-            {/* <input
-              id="name"
-              name="name"
-              type="text"
-              className={`${"form__input"} ${errors.name ? "form__input_error" : ""}`}
-              placeholder="Имя"
-              value={values.name || ""}
-              onChange={handleChange}
-              minLength="2"
-              maxLength="30"
-              required
-            /> */}
             <FioSuggestions
+              inputProps={{
+                placeholder: "Имя",
+                required: true,
+                autoFocus: true,
+              }}
               selectOnBlur="true"
-              //   containerClassName={`${"form__input"} ${errors.email ? "form__input_error" : ""}`}
               containerClassName="form__input"
               id="name"
               name="name"
-              type="text"
+              type="NAME"
               token="469678a76f51fa567ff874967776f95d2082fdfc"
-              defaultQuery="Имя"
               value={name}
               onChange={setName}
-              minLength="2"
-              maxLength="30"
-              required
             />
-            {/* <span className={`${"form__validation-message"} ${errors.name ? "form__validation-message_active" : ""}`}>
-              {errors.name}
-            </span> */}
           </div>
           <div className="input__container input__container_phone">
             <PhoneInput
+              disableDropdown
+              disableSearchIcon
+              enableAreaCodes={false}
+              onlyCountries={["ru"]}
               id="phone"
               name="phone"
               type="text"
@@ -88,39 +64,23 @@ function Form() {
               value={phone}
               onChange={setPhone}
             />
-            {/* <span className={`${"form__validation-message"} ${errors.phone ? "form__validation-message_active" : ""}`}>
-              {errors.phone}
-            </span> */}
           </div>
           <div className="input__container">
-            {/* <input
-              id="email"
-              name="email"
-              type="email"
-              className={`${"form__input"} ${errors.email ? "form__input_error" : ""}`}
-              placeholder="Почта"
-              value={values.email || ""}
-              onChange={handleChange}
-              required
-            /> */}
             <EmailSuggestions
               selectOnBlur="true"
               containerClassName="form__input"
-              //   containerClassName={`${"form__input"} ${errors.email ? "form__input_error" : ""}`}
               id="email"
               name="email"
               type="email"
               token="469678a76f51fa567ff874967776f95d2082fdfc"
               value={email}
               onChange={setEmail}
-              minLength="8"
-              maxLength="30"
-              required
-              defaultQuery="Почта"
+              inputProps={{
+                placeholder: "Почта",
+                required: true,
+                autoFocus: true,
+              }}
             />
-            {/* <span className={`${"form__validation-message"} ${errors.email ? "form__validation-message_active" : ""}`}>
-              {errors.email}
-            </span> */}
           </div>
           <div className="form__add">
             <button className="form__add-file form__add-file_button" type="button">
