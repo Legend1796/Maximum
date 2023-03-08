c08d0e50fe5c65e5af0e77001f008d292c7715d0
Igor Manukhov <m_igor97@mail.ru>
2023-03-08 21:19:31 +0300

    footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,26 +3,40 @@ import tg from "../../images/icons/tg.svg";
 import wa from "../../images/icons/wa.svg";
 import mail from "../../images/icons/mail.svg";
 import geo from "../../images/icons/geo.svg";
+import tg_tablet from "../../images/icons/tg_tablet.svg";
+import wa_tablet from "../../images/icons/wa_tablet.svg";
 
-function Footer() {
+function Footer({ pageWidth }) {
   return (
     <section className="footer">
       <div className="footer__container">
         <a href="#" className="footer__social">
-          <img src={tg} alt="telegram" className="footer__tg" />
+          {pageWidth > 1430 ? (
+            <img src={tg} alt="telegram" className="footer__icon" />
+          ) : (
+            <img src={tg_tablet} alt="telegram" className="footer__icon" />
+          )}
+
           <p className="footer__social-text">@maximum_log</p>
         </a>
         <a href="#" className="footer__social">
-          <img src={wa} alt="watsApp" className="footer__wa" />
+          {pageWidth > 1430 ? (
+            <img src={wa} alt="watsApp" className="footer__icon" />
+          ) : (
+            <img src={wa_tablet} alt="watsApp" className="footer__icon" />
+          )}
           <p className="footer__social-text">+7 (812) 200 46 43</p>
         </a>
         <a href="#" className="footer__social">
-          <img src={mail} alt="mail" className="footer__mail" />
+          <img src={mail} alt="mail" className="footer__icon" />
           <p className="footer__social-text">info@max-imum.ru</p>
         </a>
         <a href="#" className="footer__social">
-          <img src={geo} alt="geo" className="footer__geo" />
-          <p className="footer__social-text">Санкт-Петербург, ул. Ольги Берггольц, д 36, офис 1</p>
+          <img src={geo} alt="geo" className="footer__icon" />
+          <p className="footer__social-text">
+            Санкт-Петербург, <br />
+            ул. Ольги Берггольц, д 36, офис 1
+          </p>
         </a>
       </div>
     </section>
